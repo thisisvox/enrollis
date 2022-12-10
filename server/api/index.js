be646586c58664279a28c9047734ecb8ffe8abea
salma <salma.zahidi0320@gmail.com>
2022-12-10 22:17:10 +0100

    modified index.js

---
+++ server/api/index.js
@@ -0,0 +1,22 @@
+const apirouter = require("express").Router();
+
+const enrollRouter = require("./routes/enroll")
+const handoutRouter = require("./routes/handout")
+const packageRouter = require("./routes/package")
+const sessionRouter = require("./routes/session")
+const tutorRouter = require("./routes/tutor")
+//const usersRouter = require("./routes/users")
+//const studentRouter = require("./routes/student")
+
+
+apirouter.use('/', enrollRouter)
+apirouter.use('/', handoutRouter)
+apirouter.use('/', packageRouter)
+apirouter.use('/', sessionRouter)
+apirouter.use('/', tutorRouter)
+//apirouter.use('/', usersRouter)
+//apirouter.use('/', studentRouter)
+
+
+
+module.exports = apirouter
