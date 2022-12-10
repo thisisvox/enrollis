98d145bc9bd7e3597b5d1524d65b0ab1d0561635
salma <salma.zahidi0320@gmail.com>
2022-12-10 20:34:08 +0100

    removed comment from package

--- server/api/controllers/package.js
+++ server/api/controllers/package.js
@@ -1,4 +1,4 @@
-/*const express = require("express");
+const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
@@ -85,7 +85,7 @@ app.post("/package", async (req, res) => {
   app.listen(5000, () => {
       console.log("server has started on port 5000");
   });
-*/
+
 
 
 
