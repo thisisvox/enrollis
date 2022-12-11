77c8214be0253cf2fe93a7424c65789a84c819fa
Alae Hidad <a.hidad@aui.ma>
2022-12-11 17:24:12 +0100

    handout route

--- server/api/routes/handout.js
+++ server/api/routes/handout.js
@@ -1,94 +1,64 @@
-/*const express = require("express");
-const app = express();
-const cors = require("cors");
-const pool = require("./db");
+const { Router } = require('express');
 
-app.post("/handout", async (req, res) => {
+const router = Router();
+
+
+//routes
+//create handout -post: adding 
+router.post ("/handout", async(req,res)=>{
     try {
-      const {pack_type, 
-        test_title, 
-        pack_price, 
-        pack_n_session, 
-        pack_sdate, 
-        pack_edate, 
-        pack_days, 
-        pack_stime, 
-        pack_etime } = req.body;
-      const newPackage = await pool.query(
-        "INSERT INTO package (pack_type, test_title, pack_price, pack_n_session, pack_sdate,pack_edate, pack_days, pack_stime, pack_etime ) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *",
-        [pack_type, test_title, pack_price, pack_n_session, pack_sdate,pack_edate, pack_days, pack_stime, pack_etime]
-      );
-  
-      res.json(newPackage.rows[0]);
+        const {
+            doc_title,
+            doc_description,
+            sess_id,
+            pack_id
+            } = req.body;
+            const newHandout = await pool.query("INSERT INTO handout (doc_title, doc_description, sess_id, pack_id) values ($1,$2,$3,$4) returning *", [doc_title, doc_description, sess_id, pack_id]);
+            res.json(newHandout.rows[0]);
     } catch (err) {
-      console.error(err.message);
+        console.error(err.message);
     }
-  });
-
-  app.get("/package", async (req, res) => {
+})
+// get all handouts
+router.get("/handout", async(req,res)=>{
     try {
-      const allPackages = await pool.query("SELECT * FROM package");
-      res.json(allPackages.rows);
+      const allHandouts = await pool.query("SELECT * FROM handout");
+      res.json(allHandouts.rows);
     } catch (err) {
-      console.error(err.message);
+        console.error(err.message);
     }
-  });
+})
 
-  app.get("/package/:id", async (req, res) => {
+// get a handout
+router.get("/handout/:id", async(req,res)=>{
     try {
-      const { id } = req.params;
-      const package = await pool.query("SELECT * FROM package WHERE pack_id = $1", [
-        id
-      ]);
-  
-      res.json(package.rows[0]);
+        const {doc_id} = req.params;
+        const handout = await pool.query("SELECT* FROM handout WHERE doc_id = $1", [doc_id]);
+        res.json(handout.rows[0]);
     } catch (err) {
-      console.error(err.message);
+        console.error(err.message); 
     }
-  });
-  
-  app.put("/packages/:id", async (req, res) => {
+})
+//update a handout
+router.put("/handout/:id", async(req,res)=> {
     try {
-      const { id } = req.params;
-      const {pack_type, 
-        test_title, 
-        pack_price, 
-        pack_n_session, 
-        pack_sdate, 
-        pack_edate, 
-        pack_days, 
-        pack_stime, 
-        pack_etime} = req.body;
-      const updateTodo = await pool.query(
-        "UPDATE package SET pack_type = $1, test_title = $2, pack_price = $3, pack_n_session = $4, pack_sdate = $5, pack_edate = $6, pack_days = $7, pack_stime = $8, pack_etime = $9 WHERE todo_id = $2",
-        [description, id]
-      );
-  
-      res.json("Package was updated!");
+      const {handout_id} = req.params;
+      const {doc_title, doc_description, sess_id, pack_id} = req.body;
+      const updateHandout = await pool.query("UPDATE handout SET (doc_title = $1, doc_link =$2, sess_id =$3, pack_id=$4) WHERE doc_id = $6", [doc_title, doc_description, sess_id, pack_id, sess_id]);
+      res.json("Handout was updated successfully");
     } catch (err) {
-      console.error(err.message);
+        console.error(err.message);
     }
-  });
-
-  app.delete("/package/:id", async (req, res) => {
+})
+// delete a handout
+router.delete("/session/:id", async(req,res)=>{
     try {
-      const { id } = req.params;
-      const deletePackage = await pool.query("DELETE FROM package WHERE todo_id = $1", [
-        id
-      ]);
-      res.json("Package was deleted!");
+       const{doc_id} = req.params;
+       const deleteHandout = await pool.query("DELETE FROM handout WHERE doc_id= $1", [doc_id]);
+       res.json("Handout was deleted successfully");
     } catch (err) {
-      console.log(err.message);
+        console.error(err.message);
     }
-  });
-  
-  app.listen(5000, () => {
-      console.log("server has started on port 5000");
-  });*/
-
-
-
-
-
-
-  
+})
+    
+module.exports = router;
