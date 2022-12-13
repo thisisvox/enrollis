11101eced2c6409a398640a04c653889179f46d5
salma <salma.zahidi0320@gmail.com>
2022-12-13 16:04:30 +0100

    added tutor page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import ThemeProvider from './theme';
 import Header from './components/header/Header';
 import Nav from './components/nav';
+import ScrollToTop from './components/scroll-to-top';
+import UserPage from './pages/UserPage';
 import { useState } from 'react';
 import { BrowserRouter, Outlet, Router } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 // @mui
 import { styled } from '@mui/material/styles';
 
@@ -34,13 +37,16 @@ function App() {
   const [open, setOpen] = useState(false);
   return (
     <ThemeProvider>
+      <HelmetProvider>
       <BrowserRouter>
       <StyledRoot>
+        <ScrollToTop/>
         <Header onOpenNav={() => setOpen(true)} />
       <Nav openNav={open} onCloseNav={() => setOpen(false)} />
-      
+      <UserPage/>
     </StyledRoot>
       </BrowserRouter>
+      </HelmetProvider>
     </ThemeProvider>
   )
 }
